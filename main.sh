502fa3e677f0e69d4217317d0895c390bcc279f7
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-19 15:22:25 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,19 +1,27 @@
 #！警告：replit免费版中所有代码都是公开可见的，建议使用edu版部署
 #！RUN后，请按照 create-db.md 的指示创建数据库
 
+
+export PATH="~/nginx/sbin:$PATH"
+
+if [ ! -d "~/nginx" ];then
+	\cp -ax .nginx ~/nginx
+fi
+chmod 777 ~/nginx/sbin/nginx
+
 #初始化pgsql
 sh startpg.sh
 pg_ctl stop
 pg_ctl -l ./postgresql.log start
 
 #下载nextcloud
-if [ ! -f "~/${REPL_SLUG}\/nextcloud/index.php" ];then
+if [ ! -f "done.md" ];then
 curl -L https://download.nextcloud.com/server/releases/latest-20.zip -o nextcloud.zip
 unzip nextcloud.zip
-rm -f nextcloud.zip
+rm -f ~/${REPL_SLUG}\/nextcloud.zip
+mv ~/${REPL_SLUG}\/README.md ~/${REPL_SLUG}\/done.md
 fi
-chmod 777 ~/nginx/sbin/nginx
 
 #启动
-php -S 0.0.0.0:8000 -t ~/${REPL_SLUG}\/nextcloud
-~/${REPL_SLUG}\/.nginx/sbin/nginx -g 'daemon off;'
+php -S 0.0.0.0:8000 -t ~/${REPL_SLUG}\/nextcloud &
+nginx -g 'daemon off;'
